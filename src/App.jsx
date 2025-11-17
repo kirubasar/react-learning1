52276c2df1d466d56b0274644fef378c6a64cbf5
kirubasar <kiruba.sri.official@gmail.com>
2025-11-17 12:51:46 +0530

    import Welcome component inside the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Welcome from './Welcome'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Welcome/>
+    </div>
   )
 }
 
---
+++ src/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div> <h2>Welcome kiruba's Farm</h2></div>
+  )
+}
+
+export default Welcome
